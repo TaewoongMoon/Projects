9914672e90a4773f54878958b78260cd8ecb7440
TaewoongMoon <taewoongmoon1@gmail.com>
2023-02-04 14:01:41 +0900

    Instagram page

--- zero_base/test.js
+++ zero_base/test.js
@@ -1,20 +1,12 @@
-const originalArray = ['banana', 'strawberry', 'grape', 'strawberry'];
-
-const newArray = originalArray.map((fruits) =>
-	fruits === 'strawberry' ? 'kiwi' : fruits,
-);
-console.log(newArray);
-
-for (let i = 0; i < originalArray.length; i++) {
-	const newArray = [];
-	if (originalArray[i] === 'strawberry') {
-		newArray.push('kiwi');
-	} else {
-		newArray.push(originalArray[i]);
-	}
+function Person(name, gender) {
+	this.name = name;
+	this.gender = gender;
+	console.log('this', this);
 }
 
-function fruitsCount(array, fruit) {
-	const countArray = array.filter((data) => data === fruit);
-	return countArray.length;
+function Student(name, gender, school) {
+	this.school = school;
+	Person.call(this, name, gender);
 }
+
+var by = new Student('보영', 'female', '단국대');
