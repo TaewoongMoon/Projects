dd6875bab6281ca19416d119a2383716ddeab4ca
TaewoongMoon <taewoongmoon1@gmail.com>
2023-02-09 20:19:57 +0900

    DOM event practice

--- OnlinePractice/vanilla_prep_diagnosis.js
+++ OnlinePractice/vanilla_prep_diagnosis.js
@@ -3,3 +3,16 @@ for (let i = 0; i < 5; i++) {
 	p.textContent = '안녕하세요?, 문태웅입니다.';
 	document.body.appendChild(p);
 }
+const array = [];
+
+for (var i = 0; i < 5; i++) {
+	array.push(function () {
+		console.log(i);
+	});
+}
+
+for (let i = 0; i < 5; i++) {
+	array.push(function () {
+		console.log(i);
+	});
+}
