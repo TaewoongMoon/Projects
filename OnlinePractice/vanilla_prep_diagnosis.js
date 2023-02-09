26d51ec3240ce725f06deebeb6550f060c46f0b4
TaewoongMoon <taewoongmoon1@gmail.com>
2023-02-09 13:15:34 +0900

    DOM Practice

--- OnlinePractice/vanilla_prep_diagnosis.js
+++ OnlinePractice/vanilla_prep_diagnosis.js
@@ -1,28 +1,5 @@
-// number1 (currying 관련문제였는듯)
-function add(num) {
-	return function (num2) {
-		return function (num3) {
-			return num + num2 + num3;
-		};
-	};
+const p = document.createElement('p');
+for (let i = 0; i < 5; i++) {
+	p.textContent = '안녕하세요?, 문태웅입니다.';
+	document.body.appendChild(p);
 }
-
-const six = add(1)(2)(3);
-const ten = add(2)(3)(5);
-console.log(six, ten);
-
-// number2
-
-const data1 = { a: 1, b: 2, c: 5 };
-
-function inverse(collection) {
-	const result = {};
-	for (data in collection) {
-		stringKey = collection[data].toString();
-		result[stringKey] = data;
-	}
-	return result;
-}
-
-const result1 = inverse(data1);
-console.log(result1);
