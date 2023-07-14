f26a3dd33f5df50ccaac7ad4aefc30eb126426ff
TaewoongMoon <taewoongmoon1@gmail.com>
2023-07-14 22:56:57 +0900

    Day 3 Practice

--- udemy/01-Fundamentals-Part-1/starter/script.js
+++ udemy/01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,9 @@ let js = 'amazing';
 let firstName = 'Jonas';
 
 console.log(firstName);
+
+// Varaible name convention
+let jonas_matilda = 'JM';
+let $function = 27;
+
+console.log(typeof $function);
---
+++ udemy/Assignments/assignments.js
@@ -0,0 +1,48 @@
+let country = 'South Korea';
+let continent = 'Asia';
+let population = '45 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// let, const, and var
+language = 'Korean';
+
+// Basic Operators
+population = 45000000;
+console.log(Math.round(population / 2));
+console.log(population++);
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+let averagePopulation = 33000000;
+console.log(population <= averagePopulation);
+
+const description =
+	country +
+	' ' +
+	'is in' +
+	' ' +
+	continent +
+	', and its' +
+	' ' +
+	population +
+	' ' +
+	'people speak in' +
+	' ' +
+	language;
+console.log(description);
+
+// multi line string을 만들 때는 `` (백틱을 사용하는것이 유용하다).
+console.log(`string
+with
+multiple lines`);
