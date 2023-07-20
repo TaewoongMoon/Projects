cfc805c0bb0ad2c83c49514160817d02d8b62298
TaewoongMoon <taewoongmoon1@gmail.com>
2023-07-20 13:37:40 +0900

    Practice Day6

--- udemy/Assignments/assignments.js
+++ udemy/Assignments/assignments.js
@@ -63,3 +63,24 @@ if (numNeighbours == 1) {
 } else {
 	console.log('No borders');
 }
+
+switch (language) {
+	case 'chinese':
+	case 'mandarin':
+		console.log('Most number of native speakers');
+		break;
+	case 'spanish':
+		console.log('2nd place in number of native speakers');
+		break;
+	case 'english':
+		console.log('3rd place');
+		break;
+	case 'hindi':
+		console.log('Number 4');
+		break;
+	case 'arabic':
+		console.log('5th most spoken language');
+		break;
+	default:
+		console.log('Great language too :D');
+}
