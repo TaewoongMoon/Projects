d3d05ebfa239e69f24750e0f3ed98411267cfe10
TaewoongMoon <taewoongmoon1@gmail.com>
2023-07-27 17:02:17 +0900

    Practice Day13

--- udemy/05-Guess-My-Number/starter/script.js
+++ udemy/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+console.log(
+  document.querySelector('input').addEventListener('input', event => {
+    console.log(event.target.value);
+  })
+);
