c85c03c9d5ec40ecf669888211ae2841cb195fa2
TaewoongMoon <taewoongmoon1@gmail.com>
2023-07-29 04:22:19 +0900

    Practice Day15

--- udemy/06-Modal/starter/script.js
+++ udemy/06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
