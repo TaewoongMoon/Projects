c67db6496fe3906716baec69e0be023e1fa9a9bf
TaewoongMoon <taewoongmoon1@gmail.com>
2023-08-02 14:02:28 +0900

    Practice Day16

--- udemy/07-Pig-Game/starter/script.js
+++ udemy/07-Pig-Game/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Selecting Elements
+const scoreZeroEl = document.querySelector('#score--0');
+const scoreOneEl = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+scoreZeroEl.textContent = 0;
+scoreOneEl.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', () => {
+  const randNumber = Math.random();
+  console.log(randNumber);
+});
