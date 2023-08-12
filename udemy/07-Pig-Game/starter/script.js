6f6a03efed7974faee4e3c49eeaeb90d76d063b0
TaewoongMoon <taewoongmoon1@gmail.com>
2023-08-12 12:49:44 +0900

    Day 17 Practice

--- udemy/07-Pig-Game/starter/script.js
+++ udemy/07-Pig-Game/starter/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const scoreZeroEl = document.querySelector('#score--0');
 const scoreOneEl = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,7 +16,40 @@ scoreZeroEl.textContent = 0;
 scoreOneEl.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 btnRoll.addEventListener('click', () => {
-  const randNumber = Math.random();
+  const randNumber = Math.ceil(Math.random() * 6);
+  diceEl.classList.remove('hidden');
   console.log(randNumber);
+  diceEl.src = `./dice-${randNumber}.png`;
+  if (randNumber !== 1) {
+    currentScore += randNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', () => {
+  // 1. Add current score to active player's score
+
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. Check if the score is >= 100
+  // 3. If the score is < 100 switch the player
+  switchPlayer();
 });
